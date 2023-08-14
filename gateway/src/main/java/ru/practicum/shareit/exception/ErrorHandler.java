1ef9978c32aadb3c319c7d02f7567728d4332234
FokuZz <foxsus@yandex.ru>
2023-08-14 19:11:13 +0300

    feat: сделал всю логику

---
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -0,0 +1,18 @@
+package ru.practicum.shareit.exception;
+
+import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ValidationException;
+
+@RestControllerAdvice
+public class ErrorHandler2 {
+
+
+}
---
+++ gateway/src/main/java/ru/practicum/shareit/validation/ValidationGroups.java
@@ -0,0 +1,2 @@
+package ru.practicum.shareit.validation;public class ValidationGroups {
+}
